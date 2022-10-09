a82489c8b472fd2bbfdb8dea0c8c39c9c3100d18
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-09 21:22:55 +0530

    delete the data of student using delete request

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const student = require("./db/students");
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.json());
+// using .then().catch() method
+
 // app.get("/", (req, res) => {
 //   res.send("this is root folder!");
 // });
@@ -19,15 +21,19 @@ app.use(express.json());
 
 // } )
 
-// app.post("/students", async (req, res) => {
-//   try {
-//     const user = new Student(req.body);
-//     const createUser = await user.save();
-//     res.status(201).send(createUser);
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// });
+
+// using async await method
+
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 // read the data of registered students
 app.get("/students", async (req, res) => {
   try {
@@ -37,6 +43,8 @@ app.get("/students", async (req, res) => {
     res.send(e);
   }
 });
+// get student data by using Id
+
 app.get("/students/:id", async (req, res) => {
   try {
     const _id = await req.params.id; 
@@ -50,6 +58,38 @@ app.get("/students/:id", async (req, res) => {
     res.send(e);
   }
 });
+// getting student data by using name   // not working
+// app.get("/students/:name" ,async (req,res)=>{
+//         try {
+//           const _name = await req.params.name;
+//           // const  namedata = await Student.find({name:_name});
+//           res.send(_name)
+//           // if (!_name) {
+//           //   return res.status(404).send();
+//           // } else{
+//           //   res.send(namedata)
+//           // }
+        
+//         } catch (e) {
+//           res.send(e);
+//         }
+// } )
+
+// delete the student by it id
+
+app.delete("/students/:id", async (req, res)=>{
+  try{
+       const deleteStudent = await Student.findByIdAndDelete(req.params.id);
+       if(!res.params.id){
+        return res.status(400).send();
+       }
+       res.send(deleteStudent);
+  }catch(e){
+      res.status(500).send(e);
+  }
+})
+
+
 
 app.listen(port, () => {
   console.log(`connection is setup at port ${port}`);
